2269fe97efec21f028eaba884589a4b11ce0f35f
benja-morales <benja.moralesr@duocuc.cl>
2026-04-28 11:51:42 -0400

    Add files via upload

---
+++ ms-paciente/src/test/java/com/consultorio/ms_paciente/MsPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultorio.ms_paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
